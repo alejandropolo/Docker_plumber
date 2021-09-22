e416b5c8389ad48632a7c0ffb33d3f626af23497
alejandropolo <75607565+alejandropolo@users.noreply.github.com>
2021-09-22 16:48:44 +0200

    comm

--- PlumbStranded.R
+++ PlumbStranded.R
@@ -17,16 +17,7 @@ convert_empty <- function(string){
   }
 }
 
-r$registerHook(
-  list(
-    preroute=function(){
-      tictoc::tic()
-    },
-    postroute=function(req,res){
-      end<-tictoc::toc(quiet = TRUE)
-    }
-  )
-)
+
 r$run(port=80,host="0.0.0.0")
 
 ##,getOption("plumber.docs.callback")
